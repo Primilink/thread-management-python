7bd093d58c7bf656a4d8b708f5b9eb12361c622c
Angel <angel-primi@hotmail.com>
2022-11-18 14:40:00 -0600

    added v2

--- Multiprocessing/multiprocessing_project2.py
+++ Multiprocessing/multiprocessing_project2.py
@@ -22,15 +22,17 @@ def process_image(filename, threshold):  # Function to be executed in parallel
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)  # convert to grayscale
     ret, thresh = cv.threshold(
         gray, threshold, 255, cv.THRESH_BINARY)  # threshold
-    cv.imwrite(f'{REL}processed_{filename}_{threshold}.jpg',
+    cv.imwrite(f'{REL+"result/"}processed_{filename}_{threshold}.jpg',
                thresh)  # save image
     logging.debug(f'{process_id}: Finished {filename} - {threshold}')
 
 
 if __name__ == '__main__':
-    p = Pool()  # Create a multiprocessing Pool (if no argument is given, it will use all the cores)
-    for thresh in range(0, 255, 5):  # for each threshold
-        # apply the function to the image
-        p.apply_async(process_image, args=(f'image_2.jpg', thresh))
+    # Create a multiprocessing Pool (if no argument is given, it will use all the cores)
+    p = Pool()
+    for filename in ["image.jpg", "image_2.jpg", "image_3.jpg", "image_4.jpg", "image_5.jpg"]:
+        for thresh in range(0, 256, 1):  # for each threshold
+            # apply the function to the image
+            p.apply_async(process_image, args=(filename, thresh))
     p.close()  # close the pool
     p.join()  # wait for all processes to finish
